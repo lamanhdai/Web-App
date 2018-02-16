0c27536497316fb869435f274ab38d7f229f5607
lamanhdai <lanhdai1988@gmail.com>
2018-02-16 23:59:48 +0700

    feat(bootstrap-react): apply boostrap react to layout

--- shop/src/components/Header/TopHeader.js
+++ shop/src/components/Header/TopHeader.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import { Grid, Row, Col, Clearfix } from 'react-bootstrap';
+
+const LanguageButton = [
+  {
+    url: "#",
+    photo: "images/english.png",
+    label: "English"
+  },
+  {
+    url: "#",
+    photo: "images/francais.png",
+    label: "French"
+  },
+  {
+    url: "#",
+    photo: "images/german.png",
+    label: "German"
+  }
+];
+
+const CurrencyButton = [
+  {
+    url: "#",
+    unit: "USD",
+    label: "$ - Dollar"
+  },
+  {
+    url: "#",
+    unit: "POU",
+    label: "£ - Pound"
+  },
+  {
+    url: "#",
+    unit: "EUR",
+    label: "€ - Euro"
+  },
+];
 
 const TopHeader = () => {
   return (
     <div className="header-top">
-      <div className="container">
-        <div className="row"> 
-          <div className="col-xs-6">
+      <Grid>
+        <Row> 
+          <Col xs={6}>
             <div className="dropdown block-language-wrapper">
               <a role="button" data-toggle="dropdown" data-target="#" className="block-language dropdown-toggle" href="#">
                 <img src="images/english.png" alt="language"/>
@@ -17,7 +54,8 @@ const TopHeader = () => {
                 <li role="presentation"><a role="menuitem" tabIndex="-1" href="#"><img src="images/german.png" alt="language"/> German </a></li>
               </ul>
             </div>
-            <div className="dropdown block-currency-wrapper"> <a role="button" data-toggle="dropdown" data-target="#" className="block-currency dropdown-toggle" href="#"> USD <span className="caret"></span></a>
+            <div className="dropdown block-currency-wrapper">
+              <a role="button" data-toggle="dropdown" data-target="#" className="block-currency dropdown-toggle" href="#"> USD <span className="caret"></span></a>
               <ul className="dropdown-menu" role="menu">
                 <li role="presentation"><a role="menuitem" tabIndex="-1" href="#"> $ - Dollar </a></li>
                 <li role="presentation"><a role="menuitem" tabIndex="-1" href="#"> £ - Pound </a></li>
@@ -25,8 +63,8 @@ const TopHeader = () => {
               </ul>
             </div>
             <div className="welcome-msg hidden-xs"> Default welcome msg! </div>
-          </div>
-          <div className="col-xs-6"> 
+          </Col>
+          <Col xs={6}> 
             <div className="toplinks">
               <div className="links">
                 <div className="myaccount"><a title="My Account" href="login.html"><span className="hidden-xs">My Account</span></a></div>
@@ -35,9 +73,9 @@ const TopHeader = () => {
                 <div className="login"><a title="Login" href="login.html"><span  className="hidden-xs">Log In</span></a></div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Grid>
     </div>
   );
 };
