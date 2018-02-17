fa0d08a44ff1611382cea5f5d668f2da8be47bc9
lamanhdai <lanhdai1988@gmail.com>
2018-02-17 07:53:57 +0700

    feat(dropdown-top-header): update dropdown react bootstrap

--- shop/src/components/Header/TopHeader.js
+++ shop/src/components/Header/TopHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Row, Col, Clearfix } from 'react-bootstrap';
+import { Grid, Row, Col, Clearfix, ButtonToolbar, Dropdown, MenuItem } from 'react-bootstrap';
 
 const LanguageButton = [
   {
@@ -37,31 +37,54 @@ const CurrencyButton = [
   },
 ];
 
+const LanguageDropdown = () => {
+  return (
+    <ButtonToolbar className="dropdown block-language-wrapper">
+      <Dropdown id="dropdown-language">
+        <Dropdown.Toggle className="block-language">
+          <img src="images/english.png" alt="language"/>English
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <MenuItem eventKey="1" active>
+            <img src="images/english.png" alt="language"/>English
+          </MenuItem>
+          <MenuItem eventKey="2">
+            <img src="images/francais.png" alt="language"/>French
+          </MenuItem>
+          <MenuItem eventKey="3">
+            <img src="images/german.png" alt="language"/>German
+          </MenuItem>
+        </Dropdown.Menu>
+      </Dropdown>
+    </ButtonToolbar>
+  )
+}
+
+const CurrencyDropdown = () => {
+  return (
+    <ButtonToolbar className="dropdown block-currency-wrapper">
+      <Dropdown id="dropdown-currency">
+        <Dropdown.Toggle className="block-currency">
+          USD
+        </Dropdown.Toggle>
+        <Dropdown.Menu className="">
+          <MenuItem eventKey="1" active>$ - Dollar</MenuItem>
+          <MenuItem eventKey="2">£ - Pound</MenuItem>
+          <MenuItem eventKey="3">€ - Euro</MenuItem>
+        </Dropdown.Menu>
+      </Dropdown>
+    </ButtonToolbar>
+  )
+}
+
 const TopHeader = () => {
   return (
     <div className="header-top">
       <Grid>
         <Row> 
           <Col xs={6}>
-            <div className="dropdown block-language-wrapper">
-              <a role="button" data-toggle="dropdown" data-target="#" className="block-language dropdown-toggle" href="#">
-                <img src="images/english.png" alt="language"/>
-                English<span className="caret"></span>
-              </a>
-              <ul className="dropdown-menu" role="menu">
-                <li role="presentation"><a role="menuitem" tabIndex="-1" href="#"><img src="images/english.png" alt="language"/>English</a></li>
-                <li role="presentation"><a role="menuitem" tabIndex="-1" href="#"><img src="images/francais.png" alt="language"/> French </a></li>
-                <li role="presentation"><a role="menuitem" tabIndex="-1" href="#"><img src="images/german.png" alt="language"/> German </a></li>
-              </ul>
-            </div>
-            <div className="dropdown block-currency-wrapper">
-              <a role="button" data-toggle="dropdown" data-target="#" className="block-currency dropdown-toggle" href="#"> USD <span className="caret"></span></a>
-              <ul className="dropdown-menu" role="menu">
-                <li role="presentation"><a role="menuitem" tabIndex="-1" href="#"> $ - Dollar </a></li>
-                <li role="presentation"><a role="menuitem" tabIndex="-1" href="#"> £ - Pound </a></li>
-                <li role="presentation"><a role="menuitem" tabIndex="-1" href="#"> € - Euro </a></li>
-              </ul>
-            </div>
+            <LanguageDropdown />
+            <CurrencyDropdown />
             <div className="welcome-msg hidden-xs"> Default welcome msg! </div>
           </Col>
           <Col xs={6}> 
